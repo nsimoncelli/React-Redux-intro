b11f40eb767747c55f37a20eba6da2081a014a46
Nick Simoncelli <nicksimoncelli@gmail.com>
2019-10-24 16:38:44 -0700

    half way through first lesson

--- src/reducers/clock_reducer.js
+++ src/reducers/clock_reducer.js
@@ -2,3 +2,13 @@ const DEFAULT_STATE ={
     time: new Date().toLocaleTimeString()
 }
 
+
+function clockReducer(state= DEFAULT_STATE, action){
+    switch(action.type){
+        default:
+            return state;
+            
+    }
+}
+
+export default clockReducer;
